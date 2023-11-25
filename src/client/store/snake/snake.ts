9144b111f6d0d58f12370404e2c92d9470786840
David-Lyu <lyu.david@yahoo.com>
2023-11-25 10:42:03 -0800

    Added some comments

--- src/client/store/snake/snake.ts
+++ src/client/store/snake/snake.ts
@@ -26,6 +26,7 @@ export default class Snake {
       };
 
       this.snakeBody = newSeg;
+      this.snakeBody.last!.head = newSeg;
     }
   }
 
@@ -38,5 +39,23 @@ export default class Snake {
       nextBody = nextBody.next;
       tempCoord = nextBody!.coord;
     }
+  }
+
+  /**
+   * This function checks if the head of the snake collided with its body.
+   * Returns true if it has collided and false if it did not collide
+   * @param x_coord x coord of head
+   * @param y_coord y coord of head
+   * @returns boolean
+   */
+  checkSnakeBodyCollision(x_coord: number, y_coord: number): boolean {
+    let nextBody = this.snakeBody.next;
+    while (nextBody) {
+      if (nextBody.coord[0] === x_coord && nextBody.coord[1] === y_coord) {
+        return true;
+      }
+      nextBody = nextBody.next;
+    }
+    return false;
   }
 }
