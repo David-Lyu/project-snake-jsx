7638558349649c542dc6f65213da39c8600e8c07
David-Lyu <lyu.david@yahoo.com>
2024-05-05 15:59:47 -0700

    created adding styles to app

--- src/client/routes/homepage/homepage.tsx
+++ src/client/routes/homepage/homepage.tsx
@@ -1,6 +1,9 @@
+// import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function HomePage() {
+  // const [isDisclaimerClicked, setIsDisclaimerClicked] = useState(false);
+
   return (
     <div className="start-screen container">
       <div className="row start-screen-items">
@@ -10,7 +13,7 @@ export default function HomePage() {
         <Link to="/play">Play Game</Link>
         <div className="scoreboard" id="scoreboard-start-screen"></div>
       </div>
-      <section>
+      <section className="home-page-disclaimer">
         Disclaimer: This is used only for demonstration purposes and is still in
         progress.
       </section>
