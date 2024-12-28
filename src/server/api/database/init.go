7947c703bee173f5deea120c9fd977abf3278da1
David-Lyu <lyu.david@yahoo.com>
2024-12-27 20:27:15 -0800

    init sqllite

--- src/server/api/database/init.go
+++ src/server/api/database/init.go
@@ -7,9 +7,9 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func Database(rootPath string)(*sql.DB, error) {
+func Database(rootPath string) (*sql.DB, error) {
 	db, err := getDatabase(rootPath)
-	if(err != nil) {
+	if err != nil {
 		snakeLogger.LogApp("Error", err)
 		return db, err
 	}
@@ -17,18 +17,22 @@ func Database(rootPath string)(*sql.DB, error) {
 	return db, nil
 }
 
-func getDatabase(rootPath string)(*sql.DB, error) {
+func getDatabase(rootPath string) (*sql.DB, error) {
 	//Todo: Create a root dir for go or create db by hand
 
 	var dbConnection = rootPath + "/api/database/snake_db.db"
 
-	return sql.Open("sqlite3",dbConnection)
+	return sql.Open("sqlite3", dbConnection)
 }
 
 func initDatabase(sql *sql.DB) {
 	sql.Begin()
 	//Want a sqlite file instead of
-	sql.Prepare("")
+
+	_, err := sql.Exec("CREATE TABLE IF NOT EXISTS score (id INTEGER PRIMARY KEY ASC, user TEXT NOT NULL, score INTEGER NOT NULL)")
+	if err != nil {
+		snakeLogger.LogApp("error", err)
+	}
 	// sql.1
 }
 
